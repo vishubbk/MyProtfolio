1314e8b7787f016b38942c4eec7cbd2a2adbac7d
vishubbk <vishubbkup@gmail.com>
2025-03-25 23:24:38 +0530

    #

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const connectDB = require("./db");
+const connectDB = require("./db/db");
 const queryRoutes = require("./Controllers/MainControllers");
 
 const app = express();
